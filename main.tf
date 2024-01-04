2eb7464ad61409bf921f64cc91dd5f6ef84472a6
expertjaved <36668943+expertjaved@users.noreply.github.com>
2024-01-05 01:52:00 +0530

    Update main.tf
    used module for security groups

--- main.tf
+++ main.tf
@@ -18,11 +18,24 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+  name    = "blog_new"
+
+  vpc_id            = data.aws_vpc.default.id
+  ingress_rules     = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]  # Corrected variable name from ingress_cidr_blocks to egress_cidr_blocks
+
+  egress_rules     = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]  # Reference the security group ID created by the module
 
   tags = {
     Name = "Learning Terraform"
@@ -61,4 +74,5 @@ resource "aws_security_group_rule" "blog_everything_out" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
+
 
