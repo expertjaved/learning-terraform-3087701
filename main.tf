8e57c70273bcc99d74e64676f6709928f9731b32
expertjaved <36668943+expertjaved@users.noreply.github.com>
2024-01-05 18:54:33 +0530

    ALP

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "blog_sg" {
 
   vpc_id            = data.aws_vpc.default.id
   ingress_rules     = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]  # Corrected variable name from ingress_cidr_blocks to egress_cidr_blocks
+  ingress_cidr_blocks = ["0.0.0.0/0"]  
 
   egress_rules     = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
@@ -35,10 +35,81 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]  # Reference the security group ID created by the module
+  vpc_security_group_ids = [module.blog_sg.security_group_id]  
 
   tags = {
     Name = "Learning Terraform"
+  }
+}
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_sg.vpc_id
+  subnets = module.blog_sg.public_subnets
+  security_groups = [module.blog_sg.security_group_id]
+
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      description = "HTTPS web traffic"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_blocks = ["10.0.0.0/16"]
+    }
+  }
+
+  access_logs = {
+    bucket = "my-alb-logs"
+  }
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "h1"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "Example"
   }
 }
 
@@ -74,5 +145,6 @@ resource "aws_security_group_rule" "blog_everything_out" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
+
 
 
